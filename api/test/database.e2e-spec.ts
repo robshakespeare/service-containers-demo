b43d03a21e0fd5b65a251bc64a262aa93881b25d
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-10-30 17:45:42 +0000

    Code tidy

--- api/test/database.e2e-spec.ts
+++ api/test/database.e2e-spec.ts
@@ -1,27 +1,15 @@
 import { isDatabaseAvailable } from "../src/typeOrm.config";
 
-let isSetupComplete = false;
-
-// rs-todo: probably not needed:
-beforeAll(() => {
-  isSetupComplete = true;
-});
-
-describe('Database setup', () => {
-  it('Database setup should be complete',() => {
-    expect(isSetupComplete).toBeTruthy();
-  });
-
+describe('Database', () => {
   it('Database should be available to connect', async () => {
     // If this test fails, make sure the local database is ready, see the README.md in the root of the repo for more info
     // Ultimately, run `docker-compose-local.yml` and then `yarn ensure-dbs`
+
+    // ARRANGE
     const { isDbAvailable, userCount } = await isDatabaseAvailable();
+
+    // ASSERT
     expect(isDbAvailable).toBeTruthy();
     console.debug(`Database is available, number of User records is: ${userCount}`);
   });
-});
-
-// rs-todo: probably not needed:
-afterAll(() => {
-  console.debug("THIS WAS RUN, YEH!!!!");
 });
