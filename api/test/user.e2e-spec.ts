16f4b42ce24aace5bca07cd89cad60deb3bb248d
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-10-30 19:58:07 +0000

    Completed User endpoint e2e tests

--- api/test/user.e2e-spec.ts
+++ api/test/user.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+import { v4 as uuid } from 'uuid';
 import { getRandomNumber, startAppAsync } from './e2e-utils';
 
 describe('UserController (e2e)', () => {
@@ -44,5 +45,97 @@ describe('UserController (e2e)', () => {
       lastName,
       isActive: true,
     }));
+  });
+
+  it('PUT new user with an id, does not generate an id for the user, and saves and returns the user', async () => {
+    const id = uuid();
+    const firstName = `firstName${getRandomNumber()}`;
+    const lastName = `lastName${getRandomNumber()}`;
+
+    // ACT
+    const response = await request(app.getHttpServer())
+      .put('/user')
+      .send({
+        id,
+        firstName,
+        lastName,
+      });
+
+    // ASSERT
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(expect.objectContaining({
+      id,
+      firstName,
+      lastName,
+      isActive: true,
+    }));
+  });
+
+  it('does create, read and update as expected', async () => {
+    const firstName = `firstName${getRandomNumber()}`;
+    const lastName = `lastName${getRandomNumber()}`;
+    let id: string;
+
+    // ACT & ASSERT: Create
+    {
+      const createResponse = await request(app.getHttpServer())
+        .put('/user')
+        .send({
+          firstName,
+          lastName,
+        });
+
+      expect(createResponse.statusCode).toBe(200);
+      expect(typeof createResponse.body.id).toBe("string");
+
+      id = createResponse.body.id as string;
+      expect(id).toBeDefined();
+    }
+
+    // ACT & ASSERT: Read
+    {
+      const readResponse = await request(app.getHttpServer()).get(`/user/${id}`).send();
+
+      expect(readResponse.statusCode).toBe(200);
+      expect(readResponse.body).toEqual(expect.objectContaining({
+        id,
+        firstName,
+        lastName,
+        isActive: true,
+      }));
+    }
+
+    // ACT & ASSERT: Update
+    {
+      const updateResponse = await request(app.getHttpServer())
+        .put('/user')
+        .send({
+          id,
+          firstName: `Updated ${firstName}`,
+          lastName: `Updated ${lastName}`,
+          isActive: false,
+        });
+
+      expect(updateResponse.statusCode).toBe(200);
+      expect(updateResponse.body).toEqual(expect.objectContaining({
+        id,
+        firstName: `Updated ${firstName}`,
+        lastName: `Updated ${lastName}`,
+        isActive: false,
+      }));
+    }
+
+    // ACT & ASSERT: Re-Read
+    {
+      const readResponse = await request(app.getHttpServer()).get(`/user/${id}`).send();
+
+      expect(readResponse.statusCode).toBe(200);
+      expect(readResponse.body).toEqual(expect.objectContaining({
+        id,
+        firstName: `Updated ${firstName}`,
+        lastName: `Updated ${lastName}`,
+        isActive: false,
+      }));
+    }
   });
 });
